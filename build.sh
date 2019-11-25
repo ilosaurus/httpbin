69d85ebfb505776dfaa7d364acb88525ae950b0a
muhilhams <muhilhamsyarifuddin@gmail.com>
2019-11-25 15:42:57 +0700

    Update build.sh

--- build.sh
+++ build.sh
@@ -1,5 +1,6 @@
 $IMAGE_NAME=$1
+$REGISTRY=$2
 $VERSION=$(cat version)
 docker build -t $IMAGE_NAME:$VERSION .
-docker tag $IMAGE_NAME:$VERSION devops.uangteman.com:5357/$IMAGE_NAME:$VERSION
-docker push registry.sdmoko.net:5000/$IMAGE_NAME:$VERSION
+docker tag $IMAGE_NAME:$VERSION $REGISTRY/$IMAGE_NAME:$VERSION
+docker push $REGISTRY/$IMAGE_NAME:$VERSION
