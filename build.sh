7993b3e190e316c464b4701db8fd426e98def938
muhilhams <muhilhamsyarifuddin@gmail.com>
2019-11-26 19:37:34 +0700

    Update build.sh

--- build.sh
+++ build.sh
@@ -9,18 +9,23 @@ branch="$(git rev-parse --abbrev-ref HEAD)"
 hash=$(git rev-parse --short HEAD)
 version=${cleanversion}-${branch:0:2}.${revisioncount}-${hash}
 
+echo $version
+
 #if [[ $BUILD_ENV == "PROD" ]]
 #then
 #  version=${cleanversion}-${revisioncount}
 #fi
 
-version=${BUILD_PREFIX}${version}${BUILD_SUFFIX}
+version=${BUILD_PREFIX}${version}${BUILD_SUFFIX} 
 
+echo $version
 
 DOCKER_BUILDKIT=1
 docker build --build-arg version="${version}" -t utreg:5000/httpbin-testing:"${version}" --squash .  >> /var/go/test
 docker push utreg:5000/httpbin-testing:"${version}" >> /var/go/test
-echo "$version" > version >> /var/go/test
+echo "$version" > version 
+
+echo $version
 
 #IMAGE_NAME=$1
 #REGISTRY=$registry
