e380d8162b80fabed895241b83abdb05e1650730
muhilhams <muhilhamsyarifuddin@gmail.com>
2019-11-26 12:07:00 +0700

    Update build.sh

--- build.sh
+++ build.sh
@@ -1,6 +1,30 @@
 IMAGE_NAME=$1
 REGISTRY=$registry
 VERSION=$(cat version)
-docker build -t $IMAGE_NAME:$VERSION .
-docker tag $IMAGE_NAME:$VERSION $REGISTRY/$IMAGE_NAME:$VERSION
-docker push $REGISTRY/$IMAGE_NAME:$VERSION
+
+revisioncount=$(git log --oneline | wc -l | tr -d ' ')
+projectversion=$(git describe --long --tags --always)
+cleanversion=${projectversion%%-*}
+branch="$(git rev-parse --abbrev-ref HEAD)"
+hash=$(git rev-parse --short HEAD)
+version=${cleanversion}-${branch:0:2}.${revisioncount}-${hash}
+
+if [[ $BUILD_ENV == "PROD" ]]
+then
+  version=${cleanversion}-${revisioncount}
+fi
+
+version=${BUILD_PREFIX}${version}${BUILD_SUFFIX}
+
+
+DOCKER_BUILDKIT=1 docker build --ssh default --build-arg version="${version}" -t utreg:5000/loan:"${version}" --squash .
+docker push utreg:5000/loan:"${version}"
+echo "$version" > version
+
+
+#IMAGE_NAME=$1
+#REGISTRY=$registry
+#VERSION=$(cat version)
+#docker build -t $IMAGE_NAME:$VERSION .
+#docker tag $IMAGE_NAME:$VERSION $REGISTRY/$IMAGE_NAME:$VERSION
+#docker push $REGISTRY/$IMAGE_NAME:$VERSION
